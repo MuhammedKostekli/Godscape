2a2e4aa0b54fe619bc49319930cd008d2aee48a5
Muhammed Köstekli <muhammedkostekli@gmail.com>
2022-05-17 02:51:33 +0300

    World Events Integration Completed. Basic UI Added

--- Assets/Scripts/Classes/GameManager.cs
+++ Assets/Scripts/Classes/GameManager.cs
@@ -1,20 +1,29 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] float worldEventPerSec = 120f;
+    [SerializeField] GameObject uiManager;
+
+    [SerializeField] float worldEventPerSec = 10f;
     private NetworkVariable<float> timer = new NetworkVariable<float>();
-    private NetworkVariable<bool> wEventTrigger = new NetworkVariable<bool>();
+    public NetworkVariable<bool> wEventTrigger = new NetworkVariable<bool>();
     private NetworkVariable<bool> isGameStarted = new NetworkVariable<bool>(false);
     private NetworkVariable<int> currentGameYear = new NetworkVariable<int>();
+    public NetworkList<int> worldEventList;
+    public NetworkList<int> selectedWorldEventList;
+    public NetworkVariable<int> selectedWorldEventIndex = new NetworkVariable<int>();
 
     private float gameStartTime;
+    
     [SerializeField] int gameStartYear = 3245;
     [SerializeField] int gameYearPerSec = 6;
+    [SerializeField] int worldEventCount = 20;
 
     public float minutes
     {       
@@ -44,6 +53,25 @@ public class GameManager : Singleton<GameManager>
             return wEventTrigger.Value;
         }
     }
+
+    public NetworkList<int> worldEventIndexList
+    {
+        get
+        {
+            return worldEventList; 
+        }
+    }
+    public NetworkList<int> selectedWorldEventIndexList
+    {
+        get
+        {
+            return selectedWorldEventList; 
+        }
+        set
+        {
+            selectedWorldEventList = value;
+        }
+    }
     public bool gameStatus
     {
         get
@@ -55,18 +83,50 @@ public class GameManager : Singleton<GameManager>
         }
     }
 
+    public float timerVar
+    {
+        get
+        {
+            return timer.Value;
+        }
+        set
+        {
+            timer.Value = value;
+        }
+    }
+    public int selectedWorldEvent
+    {
+        get
+        {
+            return selectedWorldEventIndex.Value;
+        }
+        set
+        {
+            selectedWorldEventIndex.Value = value;
+        }
+    }
+
+
+
+    private void Awake()
+    {
+        worldEventList = new NetworkList<int>(new List<int>());
+        selectedWorldEventList = new NetworkList<int>(new List<int>());
+    }
     private void Start()
     {
         currentGameYear.Value = gameStartYear;
     }
     private void Update()
     {
-        if (gameStatus)
+        if(NetworkManager.IsHost)
         {
-            timer.Value = Time.time - gameStartTime;
-            //Debug.Log((int)timer.Value);
-        }
-        
+            if (gameStatus)
+            {
+                timer.Value = Time.time - gameStartTime;
+                //Debug.Log((int)timer.Value);
+            }
+        }  
     }
     public void StartGame()
     {
@@ -74,19 +134,65 @@ public class GameManager : Singleton<GameManager>
         StartCoroutine(gameYears());
         StartCoroutine(worldEvents());
     }
+    public void CalculateWorldEventVoteResult()
+    {
+        /*if (selectedWorldEventIndexList.Count == PlayersManager.Instance.PlayersInGame)
+        {
+            if (NetworkManager.Singleton.IsHost)
+            {
+                Debug.Log(selectedWorldEventIndexList);
+                var templist = new List<int>();
+                foreach (int element in selectedWorldEventIndexList)
+                {
+                    templist.Add(element);
+                }
+                Debug.Log(templist.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First());
+                selectedWorldEventIndex.Value = templist.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
+                Debug.Log("World Event Occurred:" + selectedWorldEventIndex.Value);
+                uiManager.GetComponent<UIManager>().showWorldEventAnnouncement();
+            }
+
+        }*/
+        Debug.Log(selectedWorldEventIndexList.Count);
+        if (selectedWorldEventIndexList.Count >= 1)
+        {
+            if (NetworkManager.Singleton.IsHost)
+            {
+                selectedWorldEventIndex.Value = selectedWorldEventIndexList[0];
+                Debug.Log("World Event Occurred:" + selectedWorldEventIndex.Value);
+            }
+            uiManager.GetComponent<UIManager>().showWorldEventAnnouncement();
+        }
+    }
     IEnumerator worldEvents()
     {
         while (true)
         {
-            Debug.Log("routine working");
             yield return new WaitForSeconds(1);
+            Debug.Log("routine working");
             yield return new WaitUntil(() => (int)(timer.Value) % worldEventPerSec == 0);
             {
-                wEventTrigger.Value = true;
+                if (NetworkManager.IsHost)
+                {
+                    worldEventList.Clear();
+                    selectedWorldEventList.Clear();
+                    while (worldEventList.Count <= 3)
+                    {
+                        System.Random rnd = new System.Random();
+                        int index = rnd.Next(worldEventCount);
+                        if (!worldEventList.Contains(index))
+                        {
+                            worldEventList.Add(index);
+                        }
+                    }
+                    wEventTrigger.Value = true;
+                }
                 Debug.Log("world event");                             
             }
         }
     }
+
+
     IEnumerator gameYears()
     {
         while (true)
--- Assets/Scripts/Classes/UIManager.cs
+++ Assets/Scripts/Classes/UIManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
@@ -20,6 +22,52 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI gameYearText;
 
+    [SerializeField]
+    private Canvas worldEventCanvas;
+
+    [SerializeField]
+    private Canvas announcementCanvas;
+
+    [SerializeField]
+    private Canvas menuCanvas;
+
+    [SerializeField]
+    private Canvas headerCanvas;
+
+    [SerializeField]
+    private List<Button> worldEventSelectionButtons;
+
+    [SerializeField] GameObject gameManager;
+
+    private List<string> worldEventNameList = new List<string>()  {
+                        "Tsunami",
+                        "Earthquake",
+                        "Storm",
+                        "Invasion",
+                        "Invasion 2",
+                        "Civil War",
+                        "World War",
+                        "Seed of Gods",
+                        "Eon Fertilizer",
+                        "Eon Batteries",
+                        "Ring of Fire",
+                        "Hackathlon",
+                        "TradeX",
+                        "Eon Crisis",
+                        "Economical Crisis",
+                        "False Gods",
+                        "Agnosticism",
+                        "Immigration",
+                        "Cable Network",
+                        "Seed of Gods infected",
+                        "Necrosis"
+                    };
+    private bool localWorldEventTrigger = false;
+    private int currentWorldEventSelection = -1;
+    [SerializeField] float worldEventSelectionSec = 10f;
+    private float worldEventStartTime;
+
+    Coroutine worldEventRoutine = null;
 
     private void Awake()
     {
@@ -28,8 +76,14 @@ public class UIManager : MonoBehaviour
 
     private void Update()
     {
-        playersInGameText.text = $"Players in game: {PlayersManager.Instance.PlayersInGame}";
-        gameYearText.text = $"Year: {GameManager.Instance.gameYear}";
+        playersInGameText.text = $"{PlayersManager.Instance.PlayersInGame} Player";
+        gameYearText.text = $"{GameManager.Instance.gameYear}";
+
+        // World Event Check
+        if (GameManager.Instance.worldEvent && !localWorldEventTrigger)
+        {
+            StartWorldEvent();
+        }
     }
 
     private void Start()
@@ -50,7 +104,8 @@ public class UIManager : MonoBehaviour
         {
             if (NetworkManager.Singleton.StartHost())
             {
-
+                menuCanvas.GetComponent<Canvas>().enabled = false;
+                headerCanvas.GetComponent<Canvas>().enabled = true;
             }
             else
             {
@@ -62,14 +117,114 @@ public class UIManager : MonoBehaviour
         {
             if (NetworkManager.Singleton.StartClient())
             {
-                
+                menuCanvas.GetComponent<Canvas>().enabled = false;
+                headerCanvas.GetComponent<Canvas>().enabled = true;
             }
             else
             {
 
             }
         });
+
+        foreach (Button btn in worldEventSelectionButtons)
+        {
+            btn.onClick.AddListener(() =>
+            {
+
+                if (currentWorldEventSelection == -1)
+                {
+                    currentWorldEventSelection = GameManager.Instance.worldEventIndexList[int.Parse(btn.tag)];
+                    btn.image.color = Color.grey;
+                    //Debug.Log(currentWorldEventSelection);
+                }
+            });
+        }
     }
 
 
+    private void StartWorldEvent()
+    {
+        currentWorldEventSelection = -1;
+        worldEventStartTime = Time.time;
+        foreach (Button btn in worldEventSelectionButtons)
+        {
+            btn.image.color = Color.white;
+        }
+        worldEventRoutine = StartCoroutine(finishWorldEvents());
+        worldEventCanvas.GetComponent<Canvas>().enabled = true;
+        localWorldEventTrigger = true;
+        UpdateWorldEventCanvas();
+    }
+
+    private void UpdateWorldEventCanvas()
+    {
+        for(int i=0; i<worldEventSelectionButtons.Count; i++)
+        {
+            worldEventSelectionButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = worldEventNameList[GameManager.Instance.worldEventIndexList[i]];
+        }
+    }
+
+    IEnumerator finishWorldEvents()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            yield return new WaitUntil(() => (int)(Time.time - worldEventStartTime) % worldEventSelectionSec == 0);
+            {
+                if (NetworkManager.Singleton.IsHost)
+                {
+                    GameManager.Instance.wEventTrigger.Value = false;
+                    if (currentWorldEventSelection != -1)
+                    {
+                        GameManager.Instance.selectedWorldEventIndexList.Add(currentWorldEventSelection);
+                    }
+                    else
+                    {
+                        GameManager.Instance.selectedWorldEventIndexList.Add(GameManager.Instance.worldEventIndexList[1]);
+                    }
+                    localWorldEventTrigger = false;
+                    worldEventCanvas.GetComponent<Canvas>().enabled = false;
+                    Debug.Log("world event finished");
+                    StopCoroutine(worldEventRoutine);
+                    gameManager.GetComponent<GameManager>().CalculateWorldEventVoteResult();
+                }
+                else
+                {
+                    yield return new WaitForSeconds(0.5f);
+                    if (currentWorldEventSelection != -1)
+                    {
+                        GameManager.Instance.selectedWorldEventIndexList.Add(currentWorldEventSelection);
+                    }
+                    else
+                    {
+                        GameManager.Instance.selectedWorldEventIndexList.Add(GameManager.Instance.worldEventIndexList[1]);
+                    }
+                    localWorldEventTrigger = false;
+                    worldEventCanvas.GetComponent<Canvas>().enabled = false;
+                    Debug.Log("world event finished");
+                    StopCoroutine(worldEventRoutine);
+                    gameManager.GetComponent<GameManager>().CalculateWorldEventVoteResult();
+                }
+               
+            }
+        }
+    }
+
+    public void showWorldEventAnnouncement()
+    {
+        var text = "World Event is happening: \n" + worldEventNameList[GameManager.Instance.selectedWorldEventIndex.Value].ToUpper();
+        Debug.Log(text);
+        StartCoroutine(showAnnouncementRoutine(text));
+    }
+
+
+    IEnumerator showAnnouncementRoutine(string text)
+    {
+        Debug.Log(text);
+        announcementCanvas.GetComponent<Canvas>().enabled = true;
+        announcementCanvas.GetComponentInChildren<TextMeshProUGUI>().text = text;
+        yield return new WaitForSeconds(3);
+        announcementCanvas.GetComponent<Canvas>().enabled = false;
+
+    }
 }
