3b7419d3758ed0841f2f578497610422a570da11
Muhammed Köstekli <muhammedkostekli@gmail.com>
2022-06-01 01:39:49 +0300

    Demo Version Completed

--- Assets/Scripts/Classes/UIManager.cs
+++ Assets/Scripts/Classes/UIManager.cs
@@ -45,6 +45,9 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI productionResText;
 
+    [SerializeField]
+    private TextMeshProUGUI winnerText;
+
     // Canvas
     [SerializeField]
     private Canvas worldEventCanvas;
@@ -65,9 +68,33 @@ public class UIManager : MonoBehaviour
     private Canvas opponentTileActionsCanvas;
 
     [SerializeField]
+    private Canvas winnerCanvas;
+
+    [SerializeField]
     private List<Button> worldEventSelectionButtons;
 
+    [SerializeField]
+    private Button organizeEventButton;
+
+    [SerializeField]
+    private Button espionageButton;
+
+    [SerializeField]
+    private Button researchButton;
+
+    [SerializeField]
+    private Button claimWarButton;
+
+    [SerializeField]
+    private Button rumorButton;
+
     [SerializeField] GameObject gameManager;
+
+    [SerializeField] int eventCost;
+
+    public bool isActiveAction = true;
+
+    public bool isActiveOpponentAction = true;
 
     private List<string> worldEventNameList = new List<string>()  {
                         "Tsunami",
@@ -109,6 +136,38 @@ public class UIManager : MonoBehaviour
     {
         playersInGameText.text = $"{PlayersManager.Instance.PlayersInGame} Player";
         // Game is running
+        if (GameManager.Instance.gameYear >= 3345)
+        {
+            GameManager.Instance.gameStatus = false;
+            winnerCanvas.GetComponent<Canvas>().enabled = true;
+            var winnerIndex = 0;
+            var winnerTotalResource = 0;
+            for (int i = 0; i < GameManager.Instance.playerInfoListVar.Count; i++){
+                var obj = GameManager.Instance.playerInfoListVar[i];
+                var curtotal = obj.militaryPoints + obj.techPoints + obj.culturePoints + obj.tradePoints + obj.productionPoints;
+                if (i == 0)
+                {
+                    winnerTotalResource = curtotal;
+                }
+                else
+                {
+                    if(curtotal >= winnerTotalResource)
+                    {
+                        winnerIndex = i;
+                        winnerTotalResource = curtotal;
+                    }
+                }
+            }
+            if(winnerIndex == GameManager.Instance.playerIndex)
+            {
+                winnerText.text = "ERA IS COMPLETED! YOU ARE WINNER " + GameManager.Instance.playerInfoListVar[winnerIndex].godName.ToString();
+            }
+            else
+            {
+                winnerText.text = "ERA IS COMPLETED! YOU ARE LOSER " + GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].godName.ToString();
+            }
+
+        }
         if (GameManager.Instance.gameStatus)
         {
             gameYearText.text = $"{GameManager.Instance.gameYear}";
@@ -124,6 +183,30 @@ public class UIManager : MonoBehaviour
                 checkTileSelection();
             }
             
+        }
+
+        if (isActiveAction)
+        {
+            organizeEventButton.interactable = true;
+            espionageButton.interactable = true;
+            researchButton.interactable = true;
+        }
+        else
+        {
+            organizeEventButton.interactable = false;
+            espionageButton.interactable = false;
+            researchButton.interactable = false;
+        }
+
+        if (isActiveOpponentAction)
+        {
+            claimWarButton.interactable = true;
+            rumorButton.interactable = true;
+        }
+        else
+        {
+            claimWarButton.interactable = false;
+            rumorButton.interactable = false;
         }
 
         // World Event Check
@@ -186,6 +269,52 @@ public class UIManager : MonoBehaviour
                 }
             });
         }
+
+        organizeEventButton.onClick.AddListener(() =>
+        {
+            var changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].culturePoints - eventCost;
+            if(changeValue > 0 && currentSelectedTileInfo.resourceType != "Culture")
+            {
+                isActiveAction = false;
+                showPlayerActionAnnouncement("Organize Event", "Successful");
+                StartCoroutine(organizeEvent(changeValue));
+            }
+            else
+            {
+                showPlayerActionAnnouncement("Organize Event", "Failed! Not enough resource");
+            }
+        });
+
+        espionageButton.onClick.AddListener(() =>
+        {
+            var changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].tradePoints - eventCost;
+            if (changeValue > 0 && currentSelectedTileInfo.resourceType != "Trade")
+            {
+                isActiveAction = false;
+                showPlayerActionAnnouncement("Espionage", "Successful");
+                StartCoroutine(espionage(changeValue));
+            }
+            else
+            {
+                showPlayerActionAnnouncement("Espionage", "Failed! Not enough resource");
+            }
+        });
+
+        researchButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(research());
+        });
+
+        claimWarButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(claimWar());
+        });
+
+        rumorButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(rumor());
+        });
+
     }
 
     private void checkTileSelection()
@@ -297,14 +426,231 @@ public class UIManager : MonoBehaviour
         StartCoroutine(showAnnouncementRoutine(text));
     }
 
+    public void showPlayerActionAnnouncement(string actionName, string txt)
+    {
+        var text = "Action is happening: \n" + actionName + "\n" + txt;
+        Debug.Log(text);
+        StartCoroutine(showAnnouncementRoutine(text));
+    }
+
+    public void showWinWarAnnouncement(string godName, bool isWin)
+    {
+        if (isWin)
+        {
+            var text = "You WON war against " + godName + ". \n Your military resource increased.";
+            Debug.Log(text);
+            StartCoroutine(showAnnouncementRoutine(text));
+        }
+        else
+        {
+            var text = "You LOST war against " + godName + ". \n Your military resource decreased.";
+            Debug.Log(text);
+            StartCoroutine(showAnnouncementRoutine(text));
+        }
+    }
+
+    public void showRumorAnnouncement(string godName, bool isWin)
+    {
+        if (isWin)
+        {
+            var text = "Rumor is succesfully COMPLETED against " + godName + ". \n Opponent trade resource decreased.";
+            Debug.Log(text);
+            StartCoroutine(showAnnouncementRoutine(text));
+        }
+        else
+        {
+            var text = "Rumor is FAILED against " + godName;
+            Debug.Log(text);
+            StartCoroutine(showAnnouncementRoutine(text));
+        }
+    }
+
 
     IEnumerator showAnnouncementRoutine(string text)
     {
         Debug.Log(text);
         announcementCanvas.GetComponent<Canvas>().enabled = true;
         announcementCanvas.GetComponentInChildren<TextMeshProUGUI>().text = text;
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(6);
         announcementCanvas.GetComponent<Canvas>().enabled = false;
 
+    }
+
+    IEnumerator organizeEvent(int changedValue)
+    {
+        yield return new WaitForSeconds(6);
+        var obj = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex];
+        var tmp = new GamePlayer();
+        var newValue = 0;
+        switch (currentSelectedTileInfo.resourceType)
+        {
+            case "Army":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, newValue, changedValue, obj.tradePoints, obj.techPoints, obj.productionPoints, obj.happiness);
+                break;
+            case "Trade":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].tradePoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, changedValue, newValue, obj.techPoints, obj.productionPoints, obj.happiness);
+                break;
+            case "Technology":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].techPoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, changedValue, obj.tradePoints, newValue, obj.productionPoints, obj.happiness);
+                break;
+            case "Production":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].productionPoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, changedValue, obj.tradePoints, obj.techPoints, newValue, obj.happiness);
+                break;
+            default:
+                break;
+
+        }
+        GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex] = tmp;
+        isActiveAction = true;
+    }
+
+    IEnumerator espionage(int changedValue)
+    {
+        yield return new WaitForSeconds(6);
+        var obj = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex];
+        var tmp = new GamePlayer();
+        var newValue = 0;
+        switch (currentSelectedTileInfo.resourceType)
+        {
+            case "Army":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, newValue, obj.culturePoints, changedValue, obj.techPoints, obj.productionPoints, obj.happiness);
+                break;
+            case "Culture":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].culturePoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, newValue, changedValue, obj.techPoints, obj.productionPoints, obj.happiness);
+                break;
+            case "Technology":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].techPoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, obj.culturePoints, changedValue, newValue, obj.productionPoints, obj.happiness);
+                break;
+            case "Production":
+                newValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].productionPoints + eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, obj.culturePoints, changedValue, obj.techPoints, newValue, obj.happiness);
+                break;
+            default:
+                break;
+
+        }
+        GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex] = tmp;
+        isActiveAction = true;
+    }
+
+    IEnumerator research()
+    {
+        isActiveAction = false;
+        yield return new WaitForSeconds(6);
+        var changeValue = 0;
+        var obj = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex];
+        var tmp = new GamePlayer();
+        switch (currentSelectedTileInfo.resourceType)
+        {
+            case "Army":
+                changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints - eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, changeValue, obj.culturePoints, obj.tradePoints, obj.techPoints + eventCost, obj.productionPoints, obj.happiness);
+                break;
+            case "Culture":
+                changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].culturePoints - eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, changeValue, obj.tradePoints, obj.techPoints + eventCost, obj.productionPoints, obj.happiness);
+                break;
+            case "Trade":
+                changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].tradePoints - eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, obj.culturePoints, changeValue, obj.techPoints + eventCost, obj.productionPoints, obj.happiness);
+                break;
+            case "Production":
+                changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].productionPoints - eventCost;
+                tmp = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, obj.culturePoints, obj.tradePoints, obj.techPoints + eventCost, changeValue, obj.happiness);
+                break;
+            default:
+                break;
+
+        }
+        if (changeValue > 0 && currentSelectedTileInfo.resourceType != "Technology")
+        {
+            showPlayerActionAnnouncement("Research", "Successful");
+            GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex] = tmp;
+            isActiveAction = true;
+        }
+        else
+        {
+            showPlayerActionAnnouncement("Research", "Failed! Not enough resource");
+        }
+    }
+
+    IEnumerator claimWar()
+    {
+        isActiveOpponentAction = false;
+
+        var opponentIndex = currentSelectedTileInfo.godIndex;
+        var opponentArmyPoint = GameManager.Instance.playerInfoListVar[opponentIndex].militaryPoints;
+        var playerArmyPoint = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints;
+
+
+        var userWinRate = ((float)(playerArmyPoint) / (playerArmyPoint + opponentArmyPoint))*100;
+        System.Random rnd = new System.Random();
+        int value = rnd.Next(100);
+        yield return new WaitForSeconds(6);
+        if (value <= userWinRate)
+        {
+            showWinWarAnnouncement(GameManager.Instance.playerInfoListVar[opponentIndex].godName.ToString(), true);
+
+            int changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints + (int)(GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints * 0.4);
+            var obj = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex];
+            GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex] = new GamePlayer(obj.godIndex, obj.godName, changeValue, obj.culturePoints, obj.tradePoints, obj.techPoints, obj.productionPoints, obj.happiness);
+
+            changeValue = GameManager.Instance.playerInfoListVar[opponentIndex].militaryPoints - (int)(GameManager.Instance.playerInfoListVar[opponentIndex].militaryPoints * 0.4);
+            obj = GameManager.Instance.playerInfoListVar[opponentIndex];
+            GameManager.Instance.playerInfoListVar[opponentIndex] = new GamePlayer(obj.godIndex, obj.godName, changeValue, obj.culturePoints, obj.tradePoints, obj.techPoints, obj.productionPoints, obj.happiness);
+        }
+        else
+        {
+            showWinWarAnnouncement(GameManager.Instance.playerInfoListVar[opponentIndex].godName.ToString(), false);
+
+            int changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints - (int)(GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].militaryPoints * 0.4);
+            var obj = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex];
+            GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex] = new GamePlayer(obj.godIndex, obj.godName, changeValue, obj.culturePoints, obj.tradePoints, obj.techPoints, obj.productionPoints, obj.happiness);
+
+            changeValue = GameManager.Instance.playerInfoListVar[opponentIndex].militaryPoints + (int)(GameManager.Instance.playerInfoListVar[opponentIndex].militaryPoints * 0.4);
+            obj = GameManager.Instance.playerInfoListVar[opponentIndex];
+            GameManager.Instance.playerInfoListVar[opponentIndex] = new GamePlayer(obj.godIndex, obj.godName, changeValue, obj.culturePoints, obj.tradePoints, obj.techPoints, obj.productionPoints, obj.happiness);
+        }
+
+        yield return new WaitForSeconds(30);
+        isActiveOpponentAction = true;
+    }
+
+    IEnumerator rumor()
+    {
+        isActiveOpponentAction = false;
+
+        int changeValue = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex].culturePoints - 500;
+        var obj = GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex];
+        GameManager.Instance.playerInfoListVar[GameManager.Instance.playerIndex] = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, changeValue, obj.tradePoints, obj.techPoints, obj.productionPoints, obj.happiness);
+
+        var opponentIndex = currentSelectedTileInfo.godIndex;
+
+        var userWinRate = 20;
+        System.Random rnd = new System.Random();
+        int value = rnd.Next(100);
+        yield return new WaitForSeconds(6);
+        if (value <= userWinRate)
+        {
+            showRumorAnnouncement(GameManager.Instance.playerInfoListVar[opponentIndex].godName.ToString(), true);
+
+            changeValue = GameManager.Instance.playerInfoListVar[opponentIndex].tradePoints - (int)(GameManager.Instance.playerInfoListVar[opponentIndex].tradePoints * 0.5);
+            obj = GameManager.Instance.playerInfoListVar[opponentIndex];
+            GameManager.Instance.playerInfoListVar[opponentIndex] = new GamePlayer(obj.godIndex, obj.godName, obj.militaryPoints, obj.culturePoints, changeValue, obj.techPoints, obj.productionPoints, obj.happiness);
+        }
+        else
+        {
+            showRumorAnnouncement(GameManager.Instance.playerInfoListVar[opponentIndex].godName.ToString(), false);
+        }
+
+        yield return new WaitForSeconds(30);
+        isActiveOpponentAction = true;
     }
 }
